15c1f0407463dcdcbe9142a8d0c9bf362eb599ee
TomTom2019 <daless2000be@yandex.com>
2022-10-26 20:00:57 +0200

    in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios';
 
+
 const App = () => {
+  let [cars,setCars] = useState([]);
+
+  useEffect(()=>{
+    getCars();
+  },[])
+
+  const getCars = () =>{
+    axios.get('/api/getcars')
+    .then( response =>{
+      setCars(response.data)
+    })
+  }
+
 
 
   const onCarSubmit = () => {
     axios.post('/api/addcar',{
-      brand: 'Porche',
-      model: '911',
+      brand: 'Mercedez',
+      model: 'Minion',
       year: 2022,
       avail: true
     })
@@ -16,7 +30,28 @@ const App = () => {
     })
   }
 
+  const onCarRemove = () =>{
+    axios.post('/api/removecar',{
+      brand:'Porche',
 
+    })
+    .then( response =>{
+      console.log(response.data)
+      getCars();
+    })
+  }
+
+  const onCarUpdate = () => {
+    axios.post('/api/updatecar',{
+      id:'63596e6aca8ba6788aaad63f',
+      brand:'TEST',
+
+    })
+    .then( response => {
+      console.log(response.data)
+      getCars();
+    })
+  }
 
   return(
     <div className='app'>
@@ -26,6 +61,18 @@ const App = () => {
       >
         Add car to DB
       </button>
+      <hr/>
+      { cars.map((car)=>(
+        <div key={car._id}>
+          {car.brand}
+        </div>
+      ))
+      }
+      <hr/>
+      <h2>Remove element/s</h2>
+      <button onClick={()=> onCarRemove()}>Delete it !!</button>
+       <h2>Update element</h2>
+        <button onClick={()=> onCarUpdate()}>Update it !!</button>
 
     </div>
   )
--- server.js
+++ server.js
@@ -1,40 +1,83 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const mongoUri = '';
 
-const mongoUri = ''
-
-mongoose.connect(mongoUri)
+mongoose.connect(mongoUri);
 app.use(bodyParser.json());
 
 
-///// SCHEma //////
-const carSchema = mongoose.Schema({
-    brand:String,
-    model:String,
-    year:Number,
-    avail:Boolean
+////////// SCHEMA ////////
 
-})
+const carSchema = mongoose.Schema({
+    brand: String,
+    model: String,
+    year: Number,
+    avail: Boolean
+});
 
 const Car = mongoose.model('Car',carSchema)
-/////
+
+//////////////////////////
+
+
+app.get('/api/getcars',(req,res)=>{
+    Car.find({},(err,doc)=>{
+        if(err) return console.log(err);
+        res.json(doc)
+    })
+})
 
 
 app.post('/api/addcar',(req,res)=>{
-    console.log(req.body)
+    const addCar = new Car({
+        brand: req.body.brand,
+        model: req.body.model,
+        year:  req.body.year,
+        avail: req.body.avail
     })
 
+    addCar.save((err,doc)=>{
+        if(err) return console.log(err)
+        res.sendStatus(200)
+    })
+})
 
+
+app.post('/api/removecar',(req,res)=>{
+    const brand = req.body.brand;
+    Car.remove({brand:brand},(err,doc)=>{
+        if(err) return console.log(err)
+        res.json(doc)
+    })
+})
+
+
+app.post('/api/updatecar',(req,res)=>{
+    const id = req.body.id;
+    const brand = req.body.brand;
+   
+
+    Car.findById(id,(err,car)=>{
+        if(err) return console.log(err)
+        car.set({
+            brand:brand,
+           
+        })
+
+        car.save((err,doc)=>{
+            if(err) return console.log(err)
+            res.json(doc)
+        })
+    })
+})
 
 
 
 const port = process.env.POST || 3001;
 app.listen(port);
-
-  
 
 
 
