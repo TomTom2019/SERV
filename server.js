64d4401a6dfc6e5b25dd7e1ac96b40fa06ed304c
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-07-07 13:46:23 +0200

    query + set server

--- server.js
+++ server.js
@@ -13,12 +13,32 @@ app.get('/',(req,res)=>{
     `)
 });
 
-app.get('/api/user',(req,res)=>{
+//PARAMS - DONT FORGET :user/:id
+// no forget api =>http://localhost:8080/api/tony/55
+app.get('/api/:user/:id',(req,res)=>{
+    let id = req.params.id;
+    let user = req.params.user
+    res.send(`
+        <html>
+            <body>
+                <h1>The user name is ${user} and the id is ${id}</h1>
+            </body>
+        </html>
+    `)
+})
+
+// querystring = hhh.com/cat?brand=batar&year=2007
+app.get('/api/cat',(req,res)=>{
+    let brand = req.query.brand;
+    let year = req.query.year;
+
     res.send({
-        name:"tony",
-        lastname:"daless"
+        brand,
+        year
     })
 })
+
+
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT)
