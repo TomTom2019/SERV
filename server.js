67545500f69d1864c8264525619b3ad9abc88366
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-07-10 16:09:41 +0200

    update

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
-
-
+///
 const mongoUri = 'mongodb+srv://admin:testing123@cluster0.lwqgg.mongodb.net/authApp?retryWrites=true&w=majority'
 
 const client = new MongoClient(mongoUri);
@@ -14,8 +13,8 @@ app.get('/api/users',async(req,res)=>{
         const database = client.db('myApp');
         const collection = database.collection('users');
         const query = await collection.insertOne({
-            name:'Tom',
-            lastname:'Dale'
+            name:'Francis',
+            lastname:'Jones'
         })
         console.log(query)
         res.status(200).json({awesome:'yes'})
@@ -37,7 +36,7 @@ app.get('/api/users',async(req,res)=>{
 // });
 
 
-const port = process.env.POST || 3001;
+const port = process.env.POST || 3808;
 app.listen(port);
 
 /// francis8976
