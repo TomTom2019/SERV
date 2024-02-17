a8bdf43de18e5482c4bb742b750f04bd547886c4
Tom <45665177+TomTom2019@users.noreply.github.com>
2024-02-17 12:17:44 +0100

    react + server

--- server.js
+++ server.js
@@ -1,44 +1,12 @@
-// first npm init
-
 const express = require('express');
-const app = express();
+const app =  express();
 
-app.get('/',(req,res)=>{
-    res.send(`
-        <html>
-            <body>
-                <h1>Hello !!</h1>
-            </body>
-        </html>
-    `)
+app.get('/api/users',(req,res)=>{
+    res.json([
+        {id:1,username:'Francis'},
+        {id:2,username:'Steve'}
+    ])
 });
 
-//PARAMS - DONT FORGET :user/:id
-// no forget api =>http://localhost:8080/api/tony/55
-app.get('/api/:user/:id',(req,res)=>{
-    let id = req.params.id;
-    let user = req.params.user
-    res.send(`
-        <html>
-            <body>
-                <h1>The user name is ${user} and the id is ${id}</h1>
-            </body>
-        </html>
-    `)
-})
-
-// querystring = hhh.com/cat?brand=batar&year=2007
-app.get('/api/cat',(req,res)=>{
-    let brand = req.query.brand;
-    let year = req.query.year;
-
-    res.send({
-        brand,
-        year
-    })
-})
-
-
-
-const PORT = process.env.PORT || 8080
-app.listen(PORT)
+const port = process.env.PORT || 3001;
+app.listen(port)
